db6bf4b306f51554d6c665b84a6c44d5e0257dc4
Galileu Batista <gbatmobile@gmail.com>
2024-10-30 21:53:57 -0300

    Codigo de Eq 2o Grau simples

---
+++ cap02-Variaveis-Expressoes/Equacao2oGrau.py
@@ -0,0 +1,9 @@
+a = int ( input('Digite o valor para a'))
+b = int ( input('Digite o valor para b'))
+c = int ( input ( 'Digite o valor para c'))
+delta = b**2 - 4*a*c
+print  ('o Valor de delta é igual', delta)
+x1 =  (- b - delta ** (1/2)) / ( 2*a)
+x2 = (- b + delta ** (1/2)) / ( 2*a)
+print ( 'o Valor para um x é', x1 )
+print ('o valor para dois x é:', x2 )
