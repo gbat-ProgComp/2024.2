d8875287ceb9c499e2b031aab4404cbdca60d83f
Galileu Batista <gbatmobile@gmail.com>
2025-02-20 09:27:29 -0300

    pygame - v3

--- cap10-Pygame/pato-V1.py
+++ cap10-Pygame/pato-V1.py
@@ -2,7 +2,7 @@ import pygame, time, sys
 
 
 def detetaColisao():
-    colisaoX = not ((patoX > jacareX + jacareW) or (patoX < jacareX))
+    colisaoX = not ((patoX > jacareX + jacareW) or (patoX+patoW < jacareX))
     colisaoY = (patoY + patoH > jacareY)
     if colisaoX and colisaoY:
         return True
@@ -13,7 +13,6 @@ def desenhaVidas():
     while nVida > 0:
         gameDisplay.blit(patoVidaImg, (780 - 60 * nVida, 20))
         nVida -= 1
-
 
 pygame.init()
 gameDisplay = pygame.display.set_mode((800,600))
@@ -36,6 +35,12 @@ jacareX = 100
 jacareY = 300
 jacareImg = pygame.transform.scale(jacareImg, (jacareW, jacareH))
 
+clock = pygame.time.Clock()
+
+try:
+    fps = int(sys.argv[1])
+except:
+    fps = 30
 
 FIM = False
 while (not FIM) and (patoVidas > 0):
@@ -47,6 +52,10 @@ while (not FIM) and (patoVidas > 0):
             FIM = True
         elif evento.type == pygame.KEYDOWN and evento.key == pygame.K_UP:
             patoY -= 25
+    
+    keys = pygame.key.get_pressed()  
+    if keys[pygame.K_UP]:
+        patoY -= 25
 
     if detetaColisao():
         patoVidas -= 1
@@ -62,11 +71,13 @@ while (not FIM) and (patoVidas > 0):
     # Atualiza display
     pygame.display.update()
 
-    time.sleep(0.05)
+    clock.tick(fps)
 
     patoY += 5
     if patoY > 300:
         patoY = 300
+    if patoY <= 30:
+        patoY = 30
 
     patoX -= 5
     if patoX < -patoW:
